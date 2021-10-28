53271d16bab3afb4d2641c49109e02f37aa67725
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-10-28 16:59:15 +0300

    script fix 2

--- src/Service.FeeShareEngine.Writer/Scripts/SumFeeShares.sql
+++ src/Service.FeeShareEngine.Writer/Scripts/SumFeeShares.sql
@@ -1,14 +1,14 @@
 DO $$
 BEGIN
-    INSERT INTO feeshares.fee_payments ("PeriodFrom", "PeriodTo","ReferrerClientId", "Amount", "CalculationTimestamp", "Status")
-        SELECT '${PeriodFrom}', '${PeriodTo}', "ReferrerClientId", SUM("FeeShareAmountInTargetAsset"), current_timestamp,  0
+    INSERT INTO feeshares.fee_payments ("PeriodFrom", "PeriodTo","ReferrerClientId", "Amount", "CalculationTimestamp", "Status", "AssetId")
+        SELECT '${PeriodFrom}', '${PeriodTo}', "ReferrerClientId", SUM("FeeShareAmountInTargetAsset"), current_timestamp,  0, "FeeShareAsset"
         FROM feeshares.fee_shares 
         WHERE "TimeStamp" > '${PeriodFrom}' AND "TimeStamp" <= '${PeriodTo}' 
         GROUP BY "ReferrerClientId", "FeeShareAsset"
     ON CONFLICT ("PeriodFrom","PeriodTo","ReferrerClientId", "AssetId") DO UPDATE SET ("Amount", "CalculationTimestamp") = (excluded."Amount", excluded."CalculationTimestamp");
 
-    INSERT INTO feeshares.share_statistics ("PeriodFrom", "PeriodTo", "Amount", "CalculationTimestamp", "Status")
-        SELECT '${PeriodFrom}', '${PeriodTo}', SUM("Amount"), current_timestamp, 0
+    INSERT INTO feeshares.share_statistics ("PeriodFrom", "PeriodTo", "Amount", "CalculationTimestamp", "Status", "AssetId")
+        SELECT '${PeriodFrom}', '${PeriodTo}', SUM("Amount"), current_timestamp, 0, "AssetId"
         FROM feeshares.fee_payments
         WHERE feeshares.fee_payments."PeriodFrom" = '${PeriodFrom}' AND "PeriodTo" = '${PeriodTo}'
         GROUP BY "ReferrerClientId", "AssetId"
