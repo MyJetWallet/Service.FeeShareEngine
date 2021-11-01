6c6736afa51efb27af0a7b0404ef65c7eadfa320
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-11-01 14:40:59 +0200

    topicname fix

--- src/Service.FeeShareEngine.Domain.Models/Models/ReferralMapChangeMessage.cs
+++ src/Service.FeeShareEngine.Domain.Models/Models/ReferralMapChangeMessage.cs
@@ -5,7 +5,7 @@ namespace Service.FeeShareEngine.Domain.Models.Models
     [DataContract]
     public class ReferralMapChangeMessage
     {
-        public const string TopicName = "jet-wallet-fee-shares-payment";
+        public const string TopicName = "jet-wallet-feeshares-referral-change";
         
         [DataMember(Order = 1)] public string ClientId { get; set; }
         [DataMember(Order = 2)] public string FeeShareGroupId { get; set; }
