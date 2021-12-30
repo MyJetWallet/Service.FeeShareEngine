56b102e6c3d7e362e0e8de30626aa2f8953cdd94
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-12-30 10:22:41 +0200

    updated logs

--- src/Service.FeeShareEngine.Writer/Services/SharesPaymentService.cs
+++ src/Service.FeeShareEngine.Writer/Services/SharesPaymentService.cs
@@ -57,121 +57,159 @@ namespace Service.FeeShareEngine.Writer.Services
 
         public async Task TransferSettlementPayment(ShareStatEntity entity)
         {
-            if (string.IsNullOrEmpty(entity.SettlementOperationId))
-                entity.SettlementOperationId = Guid.NewGuid().ToString("N")+"|FeeShareSettlement";
-            
-            var converterSettings = GetConverterSettings();
+            try
+            {
+                if (string.IsNullOrEmpty(entity.SettlementOperationId))
+                    entity.SettlementOperationId = Guid.NewGuid().ToString("N") + "|FeeShareSettlement";
 
-            var asset = _assetsDictionary.GetAssetById(new AssetIdentity
-            {
-                BrokerId = _settingsHelper.SettingsModel.FeeShareEngineBrokerId,
-                Symbol = entity.AssetId
-            });
-            
-            var amount = (double)Math.Round(entity.Amount, asset.Accuracy, MidpointRounding.ToZero);
-            
-            if (amount <= 0)
-            {
-                entity.Status = SettlementStatus.FailedToPay;
-                entity.ErrorMessage = "Amount too small";
-                return;
+                var converterSettings = GetConverterSettings();
+
+                var asset = _assetsDictionary.GetAssetById(new AssetIdentity
+                {
+                    BrokerId = _settingsHelper.SettingsModel.FeeShareEngineBrokerId,
+                    Symbol = entity.AssetId
+                });
+
+                if (asset == null)
+                {
+                    _logger.LogError("Unable to find asset {asset}", entity.AssetId);
+                    entity.Status = SettlementStatus.FailedToPay;
+                    entity.ErrorMessage = $"Unable to find asset {entity.AssetId}";
+                    return;
+                }
+
+                var amount = (double)Math.Round(entity.Amount, asset.Accuracy, MidpointRounding.ToZero);
+
+                if (amount <= 0)
+                {
+                    entity.Status = SettlementStatus.FailedToPay;
+                    entity.ErrorMessage = "Amount too small";
+                    return;
+                }
+
+                var request = new FeeTransferRequest
+                {
+                    TransactionId = entity.SettlementOperationId,
+                    ClientId = converterSettings.BrokerAccountId,
+                    FromWalletId = converterSettings.BrokerWalletId,
+                    ToWalletId = _settingsHelper.SettingsModel.FeeShareEngineWalletId,
+                    Amount = amount,
+                    AssetSymbol = entity.AssetId,
+                    Comment = "FeeShare settlement transfer to service wallet",
+                    BrokerId = converterSettings.BrokerId,
+                    RequestSource = "FeeShareEngine"
+                };
+                var result = await _changeBalanceService.TransferFeeShareToServiceWalletAsync(request);
+                if (result.Result)
+                {
+                    entity.Status = SettlementStatus.Settled;
+                    entity.PaymentTimestamp = DateTime.UtcNow;
+                }
+                else
+                {
+                    entity.Status = SettlementStatus.FailedToPay;
+                    entity.ErrorMessage = result.ErrorMessage;
+                    _logger.LogError(
+                        "When transferring fee settlement to service wallet. Transfer request {requestJson} Transfer error {error}",
+                        JsonSerializer.Serialize(request), result.ErrorMessage);
+                }
             }
-            
-            var request = new FeeTransferRequest
+            catch (Exception e)
             {
-                TransactionId = entity.SettlementOperationId,
-                ClientId = converterSettings.BrokerAccountId,
-                FromWalletId = converterSettings.BrokerWalletId,
-                ToWalletId = _settingsHelper.SettingsModel.FeeShareEngineWalletId,
-                Amount = amount,
-                AssetSymbol = entity.AssetId,
-                Comment = "FeeShare settlement transfer to service wallet",
-                BrokerId = converterSettings.BrokerId,
-                RequestSource = "FeeShareEngine"
-            };
-            var result = await _changeBalanceService.TransferFeeShareToServiceWalletAsync(request);
-            if (result.Result)
-            {
-                entity.Status = SettlementStatus.Settled;
-                entity.PaymentTimestamp = DateTime.UtcNow;
-            }
-            else
-            {
+                _logger.LogError(e, "When transferring settlement payment id {operationId}", entity.SettlementOperationId);
                 entity.Status = SettlementStatus.FailedToPay;
-                entity.ErrorMessage = result.ErrorMessage;
-                _logger.LogError("When transferring fee settlement to service wallet. Transfer request {requestJson} Transfer error {error}", JsonSerializer.Serialize(request), result.ErrorMessage);
+                entity.ErrorMessage = e.Message;
             }
         }
         
         public async Task PayFeeToReferrers(FeePaymentEntity payment)
         {
-            if (string.IsNullOrEmpty(payment.PaymentOperationId))
-                payment.PaymentOperationId = Guid.NewGuid().ToString("N")+"|FeeSharePayment";
-            
-            var response = await _walletService.SearchClientsAsync(new SearchWalletsRequest()
+            try
             {
-                SearchText = payment.ReferrerClientId,
-                Take = 1
-            });
-            
-            var asset = _assetsDictionary.GetAssetById(new AssetIdentity
-            {
-                BrokerId = _settingsHelper.SettingsModel.FeeShareEngineBrokerId,
-                Symbol = payment.AssetId
-            });
-            
-            var referrer = response.Clients?.FirstOrDefault();
-            if (referrer == null)
-            {
-                _logger.LogError("Cannot find wallet for user {userId}", payment.ReferrerClientId);
-                payment.Status = PaymentStatus.FailedToPay;
-                payment.ErrorMessage = $"Cannot find wallet for user {payment.ReferrerClientId}";
-                return;
-            }
-            
-            var walletId = (await _walletService.GetWalletsByClient(new JetClientIdentity()
-            {
-                BrokerId = referrer.BrokerId,
-                BrandId = Program.Settings.DefaultBrand,
-                ClientId = referrer.ClientId
-            })).Wallets.First();
+                if (string.IsNullOrEmpty(payment.PaymentOperationId))
+                    payment.PaymentOperationId = Guid.NewGuid().ToString("N") + "|FeeSharePayment";
 
-            var amount = (double)Math.Round(payment.Amount, asset.Accuracy, MidpointRounding.ToZero);
-            
-            if (amount <= 0)
-            {
-                payment.Status = PaymentStatus.FailedToPay;
-                payment.ErrorMessage = "Amount too small";
-                return;
-            }
-            
-            var request = new FeeTransferRequest
-            {
-                TransactionId = payment.PaymentOperationId,
-                ClientId = _settingsHelper.SettingsModel.FeeShareEngineClientId,
-                FromWalletId = _settingsHelper.SettingsModel.FeeShareEngineWalletId,
-                ToWalletId = walletId.WalletId,
-                Amount = (double)Math.Round(payment.Amount, asset.Accuracy, MidpointRounding.ToZero),
-                AssetSymbol = payment.AssetId,
-                Comment = "FeeShares payment to referrer",
-                BrokerId = referrer.BrokerId,
-                RequestSource = "FeeShareEngine"
-            };
-            var result = await _changeBalanceService.PayFeeSharesToReferrerAsync(request);
-            payment.ReferrerWalletId = walletId.WalletId;
+                var response = await _walletService.SearchClientsAsync(new SearchWalletsRequest()
+                {
+                    SearchText = payment.ReferrerClientId,
+                    Take = 1
+                });
 
-            if (result.Result)
-            {
-                payment.Status = PaymentStatus.Paid;
-                payment.PaymentTimestamp = DateTime.UtcNow;
-                
-                await _feePaymentPublisher.PublishAsync(payment);
+                var asset = _assetsDictionary.GetAssetById(new AssetIdentity
+                {
+                    BrokerId = _settingsHelper.SettingsModel.FeeShareEngineBrokerId,
+                    Symbol = payment.AssetId
+                });
+
+                if (asset == null)
+                {
+                    _logger.LogError("Unable to find asset {asset}", payment.AssetId);
+                    payment.Status = PaymentStatus.FailedToPay;
+                    payment.ErrorMessage = $"Unable to find asset {payment.AssetId}";
+                    return;
+                }
+
+                var referrer = response.Clients?.FirstOrDefault();
+                if (referrer == null)
+                {
+                    _logger.LogError("Cannot find wallet for user {userId}", payment.ReferrerClientId);
+                    payment.Status = PaymentStatus.FailedToPay;
+                    payment.ErrorMessage = $"Cannot find wallet for user {payment.ReferrerClientId}";
+                    return;
+                }
+
+                var walletId = (await _walletService.GetWalletsByClient(new JetClientIdentity()
+                {
+                    BrokerId = referrer.BrokerId,
+                    BrandId = Program.Settings.DefaultBrand,
+                    ClientId = referrer.ClientId
+                })).Wallets.First();
+
+                var amount = (double)Math.Round(payment.Amount, asset.Accuracy, MidpointRounding.ToZero);
+
+                if (amount <= 0)
+                {
+                    payment.Status = PaymentStatus.FailedToPay;
+                    payment.ErrorMessage = "Amount too small";
+                    return;
+                }
+
+                var request = new FeeTransferRequest
+                {
+                    TransactionId = payment.PaymentOperationId,
+                    ClientId = _settingsHelper.SettingsModel.FeeShareEngineClientId,
+                    FromWalletId = _settingsHelper.SettingsModel.FeeShareEngineWalletId,
+                    ToWalletId = walletId.WalletId,
+                    Amount = (double)Math.Round(payment.Amount, asset.Accuracy, MidpointRounding.ToZero),
+                    AssetSymbol = payment.AssetId,
+                    Comment = "FeeShares payment to referrer",
+                    BrokerId = referrer.BrokerId,
+                    RequestSource = "FeeShareEngine"
+                };
+                var result = await _changeBalanceService.PayFeeSharesToReferrerAsync(request);
+                payment.ReferrerWalletId = walletId.WalletId;
+
+                if (result.Result)
+                {
+                    payment.Status = PaymentStatus.Paid;
+                    payment.PaymentTimestamp = DateTime.UtcNow;
+
+                    await _feePaymentPublisher.PublishAsync(payment);
+                }
+                else
+                {
+                    payment.Status = PaymentStatus.FailedToPay;
+                    payment.ErrorMessage = result.ErrorMessage;
+                    _logger.LogError(
+                        "When transferring fee payment to referrer. Transfer request {requestJson} Transfer error {error}",
+                        JsonSerializer.Serialize(request), result.ErrorMessage);
+                }
             }
-            else
+            catch (Exception e)
             {
+                _logger.LogError(e, "When transferring fee payment to referrer id {operationId}", payment.PaymentOperationId);
                 payment.Status = PaymentStatus.FailedToPay;
-                payment.ErrorMessage = result.ErrorMessage;
-                _logger.LogError("When transferring fee payment to referrer. Transfer request {requestJson} Transfer error {error}", JsonSerializer.Serialize(request), result.ErrorMessage);
+                payment.ErrorMessage = e.Message;
             }
         }
 
